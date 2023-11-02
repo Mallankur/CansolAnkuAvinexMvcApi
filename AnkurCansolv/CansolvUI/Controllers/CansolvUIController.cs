450fda009e3ad45d9b4962f8a036580576be5ba6
AVINEX9\ankur <ankur@avinex.in>
2023-11-02 18:02:50 +0530

    ekdfhefe

---
+++ AnkurCansolv/CansolvUI/Controllers/CansolvUIController.cs
@@ -0,0 +1,38 @@
+﻿using CansolvUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CansolvUI.Controllers
+{
+    public class CansolvUIController : Controller
+    {
+
+        Uri baseAddress = new Uri("https://localhost:7222/api");  
+        private readonly HttpClient _Client;
+        public CansolvUIController()
+        {
+                _Client = new HttpClient();
+            _Client.BaseAddress = baseAddress;  
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<UICansolve> cansolvDataList = new List<UICansolve>();
+            var eventTimeStart = "2023-10-27T08:27:58.724+00:00";
+            var eventTimeEnd = "2023-10-27T08:27:58.724+00:00";
+            var queryString = $"?EventTimestart={eventTimeStart}&EventTimeEnd={eventTimeEnd}";
+
+            HttpResponseMessage response = _Client.GetAsync(_Client.BaseAddress +
+                $"/CanSolve/GetDosCansolvData/GetV").Result;
+
+            if (response.IsSuccessStatusCode)
+            { 
+                string data = response.Content.ReadAsStringAsync().Result;
+                cansolvDataList = JsonConvert.DeserializeObject<List<UICansolve>>(data);
+
+            }
+            
+            return View(cansolvDataList);
+        }
+    }
+}
---
+++ AnkurCansolv/CansolveANK/Controllers/CanSolveController.cs
@@ -0,0 +1,99 @@
+﻿using CansolveANK.AnkurLibservises;
+using CansolveANK.CansolveModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CansolveANK.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CanSolveController : ControllerBase
+    {
+        private readonly ICan _Servises;
+        public CanSolveController(ICan servises)
+        {
+            _Servises = servises;
+        }
+        /// <summary>
+        /// @AnkurMall 
+        /// </summary>
+        /// <param name="EventTimestart"></param>
+        /// <param name="EventTimeEnd"></param>
+        /// <returns></returns>
+        /// 
+        //[Authorize]
+        [HttpGet]
+        [Route("GetAsync")]
+        public async Task<List<FilterModelcs>> GetAsync(DateTime EventTimestart, DateTime EventTimeEnd)
+        {
+            var result2 = new List<FilterModelcs>();
+            var result = await _Servises.GetByEvenTimeAsync(EventTimestart, EventTimeEnd);
+            foreach (var item in result)
+            {
+                var res = new FilterModelcs
+                {
+                    id = item.Id,
+                    TagName = item.TagName,
+                    Value = item.DoubleValue,
+                    EventTime = item.EventTime,
+
+                };
+
+
+
+
+                result2.Add(res);
+            }
+
+
+            return result2;
+
+        }
+
+        
+
+        [HttpGet]
+        [Route("GetV")]
+       public async Task<List<FilterModelcs>>GetDosCansolvData()
+        {
+            var result = new List<FilterModelcs>();
+            var res0 = await _Servises.GetAsyncValueDos();
+
+            foreach (var item in res0)
+            {
+                var res = new FilterModelcs
+                {
+                    id = item.Id,
+                    TagName = item.TagName,
+                    Value = item.DoubleValue,
+                    EventTime = item.EventTime,
+
+                };
+
+                result.Add(res);
+
+
+
+            }
+
+
+            return result;
+        }
+
+
+
+
+        [HttpPost("{StartEventTimeAvgCalculations}/{endTimeForCalculations}/{frequency}")]
+        public async Task<List<AggregationModelResult>> GetAvgValue(
+    DateTime StartEventTimeAvgCalculations,
+    DateTime endTimeForCalculations,
+    long frequency,
+    [FromBody()] string[] TagName)
+        {
+            return await _Servises.GetAvgValue(StartEventTimeAvgCalculations, endTimeForCalculations, TagName, frequency);
+        }
+
+    }
+}
